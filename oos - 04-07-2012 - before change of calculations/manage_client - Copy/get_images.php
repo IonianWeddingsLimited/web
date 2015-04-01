fe2dff872ac0004be9b2fd51b97e76d1f5d458cf
Gabriel Horatiu Petchesi <pghoratiu@gmail.com>
2015-04-01 22:47:20 +0300

    Remove old/unused directory.

--- oos - 04-07-2012 - before change of calculations/manage_client - Copy/get_images.php
+++
@@ -1,7 +0,0 @@
-<?
-
-
-$gallery_name = addslashes($client_info_record[1])." ".addslashes($client_info_record[2])." ".addslashes($client_info_record[3]);
-
-$result = $sql_command->select($database_gallery,"id,gallery_name","WHERE gallery_name='".$gallery_name."'");
-$row = $sql_command->result($result);
