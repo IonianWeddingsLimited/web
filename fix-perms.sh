2537315f9a8733e0150bb640d6a2699ed313caef
Gabriel Horatiu Petchesi <pghoratiu@gmail.com>
2015-07-26 03:34:15 +0300

    Update for the permission fixing scripts.

--- fix-perms.sh
+++ fix-perms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chmod -R 777 oos/font/unifont _po images/imageuploads ckeditor/ckfinder/userfiles
+chmod -R 777 oos/font/unifont _po images/imageuploads ckeditor/ckfinder/userfiles cache
 
